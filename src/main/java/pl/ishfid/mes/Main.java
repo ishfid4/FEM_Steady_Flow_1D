97ad8ff155e824b9fb888b270472fb5888420766
Lukasz Gondela <ishfid4@gmail.com>
2016-10-30 17:26:59 +0100

    first commit

---
+++ src/main/java/pl/ishfid/mes/Main.java
@@ -0,0 +1,11 @@
+package pl.ishfid.mes;
+
+/**
+ * Created by ishfid on 30.10.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("meh");
+    }
+}
